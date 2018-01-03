1f031ff86c27dfcd04092631453e10c7a8f2a737
sibin lal <sibinsivalal@gmail.com>
2018-01-03 10:58:20 +0530

    atttuy
    ssasa

--- cradle01.ino
+++ cradle01.ino
@@ -1,55 +1,68 @@
 #define E1 10  // Enable Pin for motor 1
 #define E2 11  // Enable Pin for motor 2
- 
+
 #define I1 8  // Control pin 1 for motor 1
 #define I2 9  // Control pin 2 for motor 1
 #define I3 12  // Control pin 1 for motor 2
 #define I4 13  // Control pin 2 for motor 2
 
-const int buttonPin = 2; 
-int buttonState = 0; 
- 
+const int PushB = 2;
+int PushBstate = 0;
+
+const int limitB = 3;
+int limitBstate = 0;
+
+
 void setup() {
- 
-    pinMode(E1, OUTPUT);
-    pinMode(E2, OUTPUT);
- 
-    pinMode(I1, OUTPUT);
-    pinMode(I2, OUTPUT);
-    pinMode(I3, OUTPUT);
-    pinMode(I4, OUTPUT);
- 
-    pinMode(buttonPin, INPUT);
+
+  pinMode(E1, OUTPUT);
+  pinMode(E2, OUTPUT);
+
+  pinMode(I1, OUTPUT);
+  pinMode(I2, OUTPUT);
+  pinMode(I3, OUTPUT);
+  pinMode(I4, OUTPUT);
+
+  pinMode(PushB, INPUT);
+  pinMode(limitB, INPUT);
 }
- 
+
 void loop() {
- 
+
+  PushBstate = digitalRead(PushB);
+  limitBstate = digitalRead(limitB);
+
+
+  if (PushBstate == HIGH)
+  {
     analogWrite(E1, 153); // Run in half speed
     analogWrite(E2, 255); // Run in full speed
- 
+
     digitalWrite(I1, HIGH);
     digitalWrite(I2, LOW);
     digitalWrite(I3, HIGH);
     digitalWrite(I4, LOW);
- 
-    delay(10000);
- 
-    // change direction
- 
-    digitalWrite(E1, LOW);
-    digitalWrite(E2, LOW);
- 
-    delay(200);
- 
-    analogWrite(E1, 255);  // Run in full speed
-    analogWrite(E2, 153);  // Run in half speed
- 
-    digitalWrite(I1, LOW);
-    digitalWrite(I2, HIGH);
-    digitalWrite(I3, LOW);
-    digitalWrite(I4, HIGH);
- 
-    delay(10000);
- 
-  buttonState = digitalRead(buttonPin);
+  }
+  else
+  {
+    if (limitB == LOW)
+    {
+      digitalWrite(E1, 0);
+      digitalWrite(E2, 0);
+
+      digitalWrite(I1, LOW);
+      digitalWrite(I2, LOW);
+      digitalWrite(I3, LOW);
+      digitalWrite(I4, LOW);
+    }
+
+  }
+
+
+
+
+
+
+
+
 }
